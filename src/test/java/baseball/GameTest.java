2c123d280caf0fc4972fe9e926d100b7dd710236
nawonbin <nwb2247@gmail.com>
2026-04-03 14:05:17 +0900

    [feature] 70p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -64,6 +64,8 @@ public class GameTest {
 
     @Test
     public void 스트라이크만_있을_경우_1_strike_0_ball() {
+        generateQuestion("123");
+        assertMatchedNumber(game.guess("120"), false, 2, 0);
 
     }
 

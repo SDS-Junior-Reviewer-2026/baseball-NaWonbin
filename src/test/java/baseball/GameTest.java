65f54f4c3ad24fb838f84b924c0ff58f1550fc4d
nawonbin <nwb2247@gmail.com>
2026-04-03 13:36:29 +0900

    [feature] 46p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -33,6 +33,7 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 
 

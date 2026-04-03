608d246ed478fd7f10ab10e5901242f42b92a078
nawonbin <nwb2247@gmail.com>
2026-04-03 13:34:55 +0900

    [refactor] 44p

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -6,65 +6,68 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-	private Game game;
+    private Game game;
 
-	@BeforeEach
-	void setUp() {
-		game = new Game();
+    @BeforeEach
+    void setUp() {
+        game = new Game();
 
-	}
+    }
 
-	@Test
-	void createGame() {
-		assertNotNull(game);
-	}
+    private void assertIllegalArgument(String guessNumber) {
+        try {
+            game.guess(guessNumber);
+            fail();
+        } catch (IllegalArgumentException e) {
 
-	@Test
-	public void 입력값이_없을_경우() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			game.guess(null);
-		});
-	}
-	
-	@Test
-	public void 입력값_자리수가_세자리가_아닐_경우() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			game.guess("12");
-		});
-	}
-	
-	@Test
-	public void 입력값에_숫자_외의_문자가_입력될_경우() {
+        }
+    }
 
-	}
-	
-	@Test
-	public void 입력값에_중복된_숫자가_입력될_경우() {
+    @Test
+    void createGame() {
+        assertNotNull(game);
+    }
 
-	}
-	
-	@Test
-	public void 숫자_세개가_전부_일치_할_경우_3_strike() {
+    @Test
+    public void throwIllegalArgumentExceptionInvalidUnput() {
+        assertIllegalArgument(null);
+        assertIllegalArgument("12");
+        assertIllegalArgument("1234");
+    }
 
-	}
-	
-	@Test
-	public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
 
-	}
-	
-	@Test
-	public void 스트라이크만_있을_경우_1_strike_0_ball() {
+    @Test
+    public void 입력값에_숫자_외의_문자가_입력될_경우() {
 
-	}
-	
-	@Test
-	public void 볼만_있을_경우_0_strike_1_ball() {
+    }
 
-	}
-	
-	@Test
-	public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
+    @Test
+    public void 입력값에_중복된_숫자가_입력될_경우() {
 
-	}
+    }
+
+    @Test
+    public void 숫자_세개가_전부_일치_할_경우_3_strike() {
+
+    }
+
+    @Test
+    public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
+
+    }
+
+    @Test
+    public void 스트라이크만_있을_경우_1_strike_0_ball() {
+
+    }
+
+    @Test
+    public void 볼만_있을_경우_0_strike_1_ball() {
+
+    }
+
+    @Test
+    public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
+
+    }
 }
